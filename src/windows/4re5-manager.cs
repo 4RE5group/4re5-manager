552b2974caca3dfd774d5e3a53b31290c44aaad3
DuckpvpTeam-OFFICIAL <71982379+canarddu38@users.noreply.github.com>
2025-08-16 00:45:21 +0200

    starting windows porting

---
+++ src/windows/4re5-manager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace com.ares
+{
+    public partial class MainWindow : Form
+    {
+        private string home_path;
+        public MainWindow()
+        {
+            home_path = Environment.GetEnvironmentVariable("userprofile") + @"\4re5 group\";
+            if (!Directory.Exists(home_path))
+            {
+                Directory.CreateDirectory(home_path);
+            }
+            this.Name = "4re5 manager";
+            this.Text = "4re5 manager - v1.0";
+            this.Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            this.MinimumSize = new Size(700, 450);
+            InitializeWebBrowser();
+        }
+
+        private void InitializeWebBrowser()
+        {
+            WebBrowser webBrowser = new WebBrowser();
+            webBrowser.Dock = DockStyle.Fill;
+            webBrowser.Size = this.Size;
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.DocumentText = "<body><a href='4re5.com'>Hello World from 4re5 manager on windows</a></body>";
+            this.Controls.Add(webBrowser);
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            // Set AUMID for current process
+            string regPath = @"Software\Classes\AppUserModelId\com.ares";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(regPath, true))
+            {
+                if (key == null)
+                {
+                    using (RegistryKey newKey = Registry.CurrentUser.CreateSubKey(regPath))
+                    {
+                        newKey.SetValue("DisplayName", "4re5 manager");
+                        newKey.SetValue("ApplicationName", "4re5 manager");
+                    }
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
