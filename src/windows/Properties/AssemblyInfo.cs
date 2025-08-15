f92339ae0451a58adfcec8c36fefb2eb97a71da3
DuckpvpTeam-OFFICIAL <71982379+canarddu38@users.noreply.github.com>
2025-08-16 00:45:21 +0200

    starting windows porting

---
+++ src/windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("4re5 Manager")]
+[assembly: AssemblyDescription("Official 4re5 app downloader & updater with security features for authentication and more")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("4re5 group")]
+[assembly: AssemblyProduct("4re5 Manager")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
