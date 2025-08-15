14bb515d5c22e46c5f865ebdd35823a106d4da87
DuckpvpTeam-OFFICIAL <71982379+canarddu38@users.noreply.github.com>
2025-08-16 01:41:11 +0200

    fixed

--- src/windows/Properties/AssemblyInfo.cs
+++ src/windows/Properties/AssemblyInfo.cs
@@ -1,18 +1,25 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("4re5 Manager")]
-[assembly: AssemblyDescription("Official 4re5 app downloader & updater with security features for authentication and more")]
-[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("Official 4re5 app downloader and updater with security features")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("4re5 group")]
 [assembly: AssemblyProduct("4re5 Manager")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyCopyright("4re5 group © 2025")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8 beta")]
+[assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
