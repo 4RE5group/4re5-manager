e23759f407acfb5a1ac3a8f945d649c603abd72d
DuckpvpTeam-OFFICIAL <71982379+canarddu38@users.noreply.github.com>
2025-08-29 00:11:10 +0200

    improved linux version

---
+++ src/linux/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// 4re5 manager configuration file
+#define VERSION "v1.7"
+#define BUILD_DATE "08/28/2025_15:54"
+#define BUILD_TYPE "beta"
+
+/* --- repo informations --- */
+#define APP_DIR		"%s/.4re5 group/"
+#define REPO_PATH	"%s/.4re5 group/repo.json"
+#define REPO_URL	"https://raw.githubusercontent.com/4RE5group/4re5-repository/refs/heads/main/repo.json"
+
+#endif
